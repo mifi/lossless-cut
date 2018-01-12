686e51f20e4edf755707ccb977c0859431f895e8
Nick O <debuggeek@gmail.com>
2018-01-11 22:25:15 -0600

    Update release script ffmpeg versions

--- scripts/ffmpeg-dl/dl.sh
+++ scripts/ffmpeg-dl/dl.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 ffmpeg_linux_ia32=https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz
 ffmpeg_linux_x64=https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
-ffmpeg_darwin_x64=http://evermeet.cx/ffmpeg/ffmpeg-3.2.7z
-ffmpeg_win32_ia32=https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-3.1.5-win32-static.zip
-ffmpeg_win32_x64=https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.1.5-win64-static.zip
-ffprobe_darwin_x64=http://evermeet.cx/ffmpeg/ffprobe-3.2.7z
+ffmpeg_darwin_x64=http://evermeet.cx/ffmpeg/ffmpeg-3.4.1.7z
+ffmpeg_win32_ia32=https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-3.4.1-win32-static.zip
+ffmpeg_win32_x64=https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.4.1-win64-static.zip
+ffprobe_darwin_x64=http://evermeet.cx/ffmpeg/ffprobe-3.4.1.7z
 
 OUT_DIR=ffmpeg-tmp/archives
 
@@ -12,7 +13,7 @@ if [ -d "$OUT_DIR" ]; then
   exit
 fi
 
-mkdir "$OUT_DIR" &&
+mkdir -p "$OUT_DIR" &&
 (cd "$OUT_DIR" &&
 wget -O ffmpeg_linux_ia32.tar.xz "${ffmpeg_linux_ia32}" &&
 wget -O ffmpeg_linux_x64.tar.xz "${ffmpeg_linux_x64}" &&
