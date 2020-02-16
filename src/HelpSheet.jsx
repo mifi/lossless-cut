59d5700f9e5e954c7b24c646938d6c9e8c6c4786
Mikael Finstad <finstaden@gmail.com>
2020-02-16 23:09:56 +0800

    update help

--- src/HelpSheet.jsx
+++ src/HelpSheet.jsx
@@ -28,6 +28,7 @@ const HelpSheet = ({ visible, onTogglePress, renderSettings }) => (
           <li><kbd>E</kbd> Cut (export selection in the same directory)</li>
           <li><kbd>C</kbd> Capture snapshot (in the same directory)</li>
           <li><kbd>+</kbd> Add cut segment</li>
+          <li><kbd>D</kbd> Delete source file</li>
           <li><kbd>BACKSPACE</kbd> Remove current cut segment</li>
         </ul>
 
