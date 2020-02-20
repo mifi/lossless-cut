542d4ef946075c1537dcf04b3459469a9102b430
Mikael Finstad <finstaden@gmail.com>
2020-02-20 10:56:18 +0800

    reduce springyness

--- src/animations.js
+++ src/animations.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const mySpring = { type: 'spring', damping: 30, stiffness: 1000 };
+export const mySpring = { type: 'spring', damping: 25, stiffness: 350 };
