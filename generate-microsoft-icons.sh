3c5429f5808b9b789ff254d24d64b4f11c084f91
Mikael Finstad <finstaden@gmail.com>
2020-03-31 15:43:20 +0800

    try to fix build

--- generate-microsoft-icons.sh
+++
@@ -1,4 +0,0 @@
-npx svg2png src/icon.svg --output=build-resources/appx/StoreLogo.png --width=50 --height=50
-npx svg2png src/icon.svg --output=build-resources/appx/Square150x150Logo.png --width=300 --height=300
-npx svg2png src/icon.svg --output=build-resources/appx/Square44x44Logo.png --width=44 --height=44
-npx svg2png src/icon.svg --output=build-resources/appx/Wide310x150Logo.png --width=620 --height=300
