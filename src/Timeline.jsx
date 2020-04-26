46a58c99bdb03b0053d0ecf3931aa6ded4f4d71e
Mikael Finstad <finstaden@gmail.com>
2020-04-26 23:23:00 +0800

    make keyframes transparent

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -245,7 +245,7 @@ const Timeline = memo(({
             ))}
 
             {shouldShowKeyframes && !areKeyframesTooClose && keyframes.map((f) => (
-              <div key={f.time} style={{ position: 'absolute', top: 0, bottom: 0, left: `${(f.time / duration) * 100}%`, marginLeft: -1, width: 1, background: 'rgba(0,0,0,1)', pointerEvents: 'none' }} />
+              <div key={f.time} style={{ position: 'absolute', top: 0, bottom: 0, left: `${(f.time / duration) * 100}%`, marginLeft: -1, width: 1, background: 'rgba(0,0,0,0.4)', pointerEvents: 'none' }} />
             ))}
           </div>
         </div>
