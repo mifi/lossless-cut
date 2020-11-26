f604ddc2402ea3118dc0cf220e7fb163ca906e30
Mikael Finstad <finstaden@gmail.com>
2020-11-26 22:21:08 +0100

    update mifi config url

--- src/mifi.js
+++ src/mifi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // eslint-disable-next-line import/prefer-default-export
 export async function loadMifiLink() {
   try {
-    const resp = await axios.get('https://mifi.no/losslesscut/config.json');
+    const resp = await axios.get('https://losslesscut.mifi.no/config.json');
     return resp.data;
   } catch (err) {
     return undefined;
