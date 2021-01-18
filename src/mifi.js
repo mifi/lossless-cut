73aec1225a3b090987e76a42e5e7731a5de32af1
Mikael Finstad <finstaden@gmail.com>
2021-01-18 21:16:10 +0100

    Improve merge descripiton #566

--- src/mifi.js
+++ src/mifi.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // eslint-disable-next-line import/prefer-default-export
 export async function loadMifiLink() {
   try {
+    // In old versions: https://mifi.no/losslesscut/config.json
     const resp = await axios.get('https://losslesscut.mifi.no/config.json');
     return resp.data;
   } catch (err) {
