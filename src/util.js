56bc6f60dfb7c43299ed2d2e49db5bb664ffcf69
Mikael Finstad <finstaden@gmail.com>
2021-07-25 13:39:02 +0700

    improve toasts by showing close button

--- src/util.js
+++ src/util.js
@@ -67,6 +67,7 @@ export const toast = Swal.mixin({
   toast: true,
   position: 'top',
   showConfirmButton: false,
+  showCloseButton: true,
   timer: 5000,
   timerProgressBar: true,
   didOpen: (self) => {
