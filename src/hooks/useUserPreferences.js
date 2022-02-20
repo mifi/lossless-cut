464f0cd544e09470d9e178c0a44fda349aff9b71
Mikael Finstad <finstaden@gmail.com>
2022-02-20 17:23:27 +0800

    fix slowness

--- src/hooks/useUserPreferences.js
+++ src/hooks/useUserPreferences.js
@@ -24,75 +24,83 @@ export default () => {
     }
   }
 
-  const [captureFormat, setCaptureFormat] = useState(configStore.get('captureFormat'));
+  function safeGetConfig(key) {
+    const rawVal = configStore.get(key);
+    if (rawVal === undefined) return undefined;
+    // NOTE: Need to clone any non-primitive in renderer, or it will become very slow
+    // I think because Electron is proxying objects over the bridge
+    return JSON.parse(JSON.stringify(rawVal));
+  }
+
+  const [captureFormat, setCaptureFormat] = useState(safeGetConfig('captureFormat'));
   useEffect(() => safeSetConfig('captureFormat', captureFormat), [captureFormat]);
-  const [customOutDir, setCustomOutDir] = useState(configStore.get('customOutDir'));
+  const [customOutDir, setCustomOutDir] = useState(safeGetConfig('customOutDir'));
   useEffect(() => safeSetConfig('customOutDir', customOutDir), [customOutDir]);
-  const [keyframeCut, setKeyframeCut] = useState(configStore.get('keyframeCut'));
+  const [keyframeCut, setKeyframeCut] = useState(safeGetConfig('keyframeCut'));
   useEffect(() => safeSetConfig('keyframeCut', keyframeCut), [keyframeCut]);
-  const [preserveMovData, setPreserveMovData] = useState(configStore.get('preserveMovData'));
+  const [preserveMovData, setPreserveMovData] = useState(safeGetConfig('preserveMovData'));
   useEffect(() => safeSetConfig('preserveMovData', preserveMovData), [preserveMovData]);
-  const [movFastStart, setMovFastStart] = useState(configStore.get('movFastStart'));
+  const [movFastStart, setMovFastStart] = useState(safeGetConfig('movFastStart'));
   useEffect(() => safeSetConfig('movFastStart', movFastStart), [movFastStart]);
-  const [avoidNegativeTs, setAvoidNegativeTs] = useState(configStore.get('avoidNegativeTs'));
+  const [avoidNegativeTs, setAvoidNegativeTs] = useState(safeGetConfig('avoidNegativeTs'));
   useEffect(() => safeSetConfig('avoidNegativeTs', avoidNegativeTs), [avoidNegativeTs]);
-  const [autoMerge, setAutoMerge] = useState(configStore.get('autoMerge'));
+  const [autoMerge, setAutoMerge] = useState(safeGetConfig('autoMerge'));
   useEffect(() => safeSetConfig('autoMerge', autoMerge), [autoMerge]);
-  const [timecodeFormat, setTimecodeFormat] = useState(configStore.get('timecodeFormat'));
+  const [timecodeFormat, setTimecodeFormat] = useState(safeGetConfig('timecodeFormat'));
   useEffect(() => safeSetConfig('timecodeFormat', timecodeFormat), [timecodeFormat]);
-  const [invertCutSegments, setInvertCutSegments] = useState(configStore.get('invertCutSegments'));
+  const [invertCutSegments, setInvertCutSegments] = useState(safeGetConfig('invertCutSegments'));
   useEffect(() => safeSetConfig('invertCutSegments', invertCutSegments), [invertCutSegments]);
-  const [autoExportExtraStreams, setAutoExportExtraStreams] = useState(configStore.get('autoExportExtraStreams'));
+  const [autoExportExtraStreams, setAutoExportExtraStreams] = useState(safeGetConfig('autoExportExtraStreams'));
   useEffect(() => safeSetConfig('autoExportExtraStreams', autoExportExtraStreams), [autoExportExtraStreams]);
-  const [askBeforeClose, setAskBeforeClose] = useState(configStore.get('askBeforeClose'));
+  const [askBeforeClose, setAskBeforeClose] = useState(safeGetConfig('askBeforeClose'));
   useEffect(() => safeSetConfig('askBeforeClose', askBeforeClose), [askBeforeClose]);
-  const [enableAskForImportChapters, setEnableAskForImportChapters] = useState(configStore.get('enableAskForImportChapters'));
+  const [enableAskForImportChapters, setEnableAskForImportChapters] = useState(safeGetConfig('enableAskForImportChapters'));
   useEffect(() => safeSetConfig('enableAskForImportChapters', enableAskForImportChapters), [enableAskForImportChapters]);
-  const [enableAskForFileOpenAction, setEnableAskForFileOpenAction] = useState(configStore.get('enableAskForFileOpenAction'));
+  const [enableAskForFileOpenAction, setEnableAskForFileOpenAction] = useState(safeGetConfig('enableAskForFileOpenAction'));
   useEffect(() => safeSetConfig('enableAskForFileOpenAction', enableAskForFileOpenAction), [enableAskForFileOpenAction]);
-  const [playbackVolume, setPlaybackVolume] = useState(configStore.get('playbackVolume'));
+  const [playbackVolume, setPlaybackVolume] = useState(safeGetConfig('playbackVolume'));
   useEffect(() => safeSetConfig('playbackVolume', playbackVolume), [playbackVolume]);
-  const [autoSaveProjectFile, setAutoSaveProjectFile] = useState(configStore.get('autoSaveProjectFile'));
+  const [autoSaveProjectFile, setAutoSaveProjectFile] = useState(safeGetConfig('autoSaveProjectFile'));
   useEffect(() => safeSetConfig('autoSaveProjectFile', autoSaveProjectFile), [autoSaveProjectFile]);
-  const [wheelSensitivity, setWheelSensitivity] = useState(configStore.get('wheelSensitivity'));
+  const [wheelSensitivity, setWheelSensitivity] = useState(safeGetConfig('wheelSensitivity'));
   useEffect(() => safeSetConfig('wheelSensitivity', wheelSensitivity), [wheelSensitivity]);
-  const [invertTimelineScroll, setInvertTimelineScroll] = useState(configStore.get('invertTimelineScroll'));
+  const [invertTimelineScroll, setInvertTimelineScroll] = useState(safeGetConfig('invertTimelineScroll'));
   useEffect(() => safeSetConfig('invertTimelineScroll', invertTimelineScroll), [invertTimelineScroll]);
-  const [language, setLanguage] = useState(configStore.get('language'));
+  const [language, setLanguage] = useState(safeGetConfig('language'));
   useEffect(() => safeSetConfig('language', language), [language]);
-  const [ffmpegExperimental, setFfmpegExperimental] = useState(configStore.get('ffmpegExperimental'));
+  const [ffmpegExperimental, setFfmpegExperimental] = useState(safeGetConfig('ffmpegExperimental'));
   useEffect(() => safeSetConfig('ffmpegExperimental', ffmpegExperimental), [ffmpegExperimental]);
-  const [hideNotifications, setHideNotifications] = useState(configStore.get('hideNotifications'));
+  const [hideNotifications, setHideNotifications] = useState(safeGetConfig('hideNotifications'));
   useEffect(() => safeSetConfig('hideNotifications', hideNotifications), [hideNotifications]);
-  const [autoLoadTimecode, setAutoLoadTimecode] = useState(configStore.get('autoLoadTimecode'));
+  const [autoLoadTimecode, setAutoLoadTimecode] = useState(safeGetConfig('autoLoadTimecode'));
   useEffect(() => safeSetConfig('autoLoadTimecode', autoLoadTimecode), [autoLoadTimecode]);
-  const [autoDeleteMergedSegments, setAutoDeleteMergedSegments] = useState(configStore.get('autoDeleteMergedSegments'));
+  const [autoDeleteMergedSegments, setAutoDeleteMergedSegments] = useState(safeGetConfig('autoDeleteMergedSegments'));
   useEffect(() => safeSetConfig('autoDeleteMergedSegments', autoDeleteMergedSegments), [autoDeleteMergedSegments]);
-  const [exportConfirmEnabled, setExportConfirmEnabled] = useState(configStore.get('exportConfirmEnabled'));
+  const [exportConfirmEnabled, setExportConfirmEnabled] = useState(safeGetConfig('exportConfirmEnabled'));
   useEffect(() => safeSetConfig('exportConfirmEnabled', exportConfirmEnabled), [exportConfirmEnabled]);
-  const [segmentsToChapters, setSegmentsToChapters] = useState(configStore.get('segmentsToChapters'));
+  const [segmentsToChapters, setSegmentsToChapters] = useState(safeGetConfig('segmentsToChapters'));
   useEffect(() => safeSetConfig('segmentsToChapters', segmentsToChapters), [segmentsToChapters]);
-  const [preserveMetadataOnMerge, setPreserveMetadataOnMerge] = useState(configStore.get('preserveMetadataOnMerge'));
+  const [preserveMetadataOnMerge, setPreserveMetadataOnMerge] = useState(safeGetConfig('preserveMetadataOnMerge'));
   useEffect(() => safeSetConfig('preserveMetadataOnMerge', preserveMetadataOnMerge), [preserveMetadataOnMerge]);
-  const [simpleMode, setSimpleMode] = useState(configStore.get('simpleMode'));
+  const [simpleMode, setSimpleMode] = useState(safeGetConfig('simpleMode'));
   useEffect(() => safeSetConfig('simpleMode', simpleMode), [simpleMode]);
-  const [outSegTemplate, setOutSegTemplate] = useState(configStore.get('outSegTemplate'));
+  const [outSegTemplate, setOutSegTemplate] = useState(safeGetConfig('outSegTemplate'));
   useEffect(() => safeSetConfig('outSegTemplate', outSegTemplate), [outSegTemplate]);
-  const [keyboardSeekAccFactor, setKeyboardSeekAccFactor] = useState(configStore.get('keyboardSeekAccFactor'));
+  const [keyboardSeekAccFactor, setKeyboardSeekAccFactor] = useState(safeGetConfig('keyboardSeekAccFactor'));
   useEffect(() => safeSetConfig('keyboardSeekAccFactor', keyboardSeekAccFactor), [keyboardSeekAccFactor]);
-  const [keyboardNormalSeekSpeed, setKeyboardNormalSeekSpeed] = useState(configStore.get('keyboardNormalSeekSpeed'));
+  const [keyboardNormalSeekSpeed, setKeyboardNormalSeekSpeed] = useState(safeGetConfig('keyboardNormalSeekSpeed'));
   useEffect(() => safeSetConfig('keyboardNormalSeekSpeed', keyboardNormalSeekSpeed), [keyboardNormalSeekSpeed]);
-  const [enableTransferTimestamps, setEnableTransferTimestamps] = useState(configStore.get('enableTransferTimestamps'));
+  const [enableTransferTimestamps, setEnableTransferTimestamps] = useState(safeGetConfig('enableTransferTimestamps'));
   useEffect(() => safeSetConfig('enableTransferTimestamps', enableTransferTimestamps), [enableTransferTimestamps]);
-  const [outFormatLocked, setOutFormatLocked] = useState(configStore.get('outFormatLocked'));
+  const [outFormatLocked, setOutFormatLocked] = useState(safeGetConfig('outFormatLocked'));
   useEffect(() => safeSetConfig('outFormatLocked', outFormatLocked), [outFormatLocked]);
-  const [safeOutputFileName, setSafeOutputFileName] = useState(configStore.get('safeOutputFileName'));
+  const [safeOutputFileName, setSafeOutputFileName] = useState(safeGetConfig('safeOutputFileName'));
   useEffect(() => safeSetConfig('safeOutputFileName', safeOutputFileName), [safeOutputFileName]);
-  const [enableAutoHtml5ify, setEnableAutoHtml5ify] = useState(configStore.get('enableAutoHtml5ify'));
+  const [enableAutoHtml5ify, setEnableAutoHtml5ify] = useState(safeGetConfig('enableAutoHtml5ify'));
   useEffect(() => safeSetConfig('enableAutoHtml5ify', enableAutoHtml5ify), [enableAutoHtml5ify]);
-  const [segmentsToChaptersOnly, setSegmentsToChaptersOnly] = useState(configStore.get('segmentsToChaptersOnly'));
+  const [segmentsToChaptersOnly, setSegmentsToChaptersOnly] = useState(safeGetConfig('segmentsToChaptersOnly'));
   useEffect(() => safeSetConfig('segmentsToChaptersOnly', segmentsToChaptersOnly), [segmentsToChaptersOnly]);
-  const [keyBindings, setKeyBindings] = useState(configStore.get('keyBindings'));
+  const [keyBindings, setKeyBindings] = useState(safeGetConfig('keyBindings'));
   useEffect(() => safeSetConfig('keyBindings', keyBindings), [keyBindings]);
 
   // NOTE! This useEffect must be placed after all usages of firstUpdateRef.current (safeSetConfig)
