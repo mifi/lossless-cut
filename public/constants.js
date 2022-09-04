0113d79e0c4259d7b33f1e42e098ced41399042e
Mikael Finstad <finstaden@gmail.com>
2022-09-04 15:04:18 +0200

    update licenses path

--- public/constants.js
+++ public/constants.js
@@ -1,7 +1,7 @@
 const homepage = 'https://mifi.no/losslesscut/';
 const githubLink = 'https://github.com/mifi/lossless-cut/';
 const getReleaseUrl = (version) => `https://github.com/mifi/lossless-cut/releases/tag/v${version}`;
-const licensesPage = 'https://mifi.no/losslesscut/licenses.txt';
+const licensesPage = 'https://losslesscut.mifi.no/licenses.txt';
 
 module.exports = {
   homepage,
