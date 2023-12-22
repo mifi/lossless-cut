cec54056a6514f217f884a1c4bd44c873397be41
Mikael Finstad <finstaden@gmail.com>
2023-12-22 15:05:31 +0800

    update mac icon
    closes #543

--- script/icon-gen.mjs
+++ script/icon-gen.mjs
@@ -9,14 +9,20 @@ const svg2png = (from, to, width, height) => sharp(from)
   })
   .toFile(to);
 
+// Linux:
 await svg2png('src/icon.svg', './icon-build/app-512.png', 512, 512);
+
+// Windows Store
 await svg2png('src/icon.svg', './build-resources/appx/StoreLogo.png', 50, 50);
 await svg2png('src/icon.svg', './build-resources/appx/Square150x150Logo.png', 300, 300);
 await svg2png('src/icon.svg', './build-resources/appx/Square44x44Logo.png', 44, 44);
 await svg2png('src/icon.svg', './build-resources/appx/Wide310x150Logo.png', 620, 300);
 
-await icongen('./src/icon.svg', './icon-build', { icns: { sizes: [512, 1024] } });
+// MacOS:
+// https://github.com/mifi/lossless-cut/issues/1820
+await icongen('./src/icon-mac.svg', './icon-build', { icns: { sizes: [512, 1024] } });
 
+// Windows ICO:
 // https://github.com/mifi/lossless-cut/issues/778
 // https://stackoverflow.com/questions/3236115/which-icon-sizes-should-my-windows-applications-icon-include
 await icongen('./src/icon.svg', './icon-build', { ico: { sizes: [16, 24, 32, 40, 48, 64, 96, 128, 256, 512] } });
