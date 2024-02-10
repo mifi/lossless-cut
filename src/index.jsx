ead7251a1848d7cb31d75ca952edbaea8f1a7aa2
Mikael Finstad <finstaden@gmail.com>
2024-02-10 18:22:14 +0800

    eanble strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MotionConfig } from 'framer-motion';
 import { enableMapSet } from 'immer';
@@ -35,11 +35,13 @@ console.log('Version', app.getVersion());
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <ErrorBoundary>
-    <Suspense fallback={<div />}>
-      <MotionConfig reducedMotion="user">
-        <App />
-      </MotionConfig>
-    </Suspense>
-  </ErrorBoundary>,
+  <StrictMode>
+    <ErrorBoundary>
+      <Suspense fallback={<div />}>
+        <MotionConfig reducedMotion="user">
+          <App />
+        </MotionConfig>
+      </Suspense>
+    </ErrorBoundary>
+  </StrictMode>,
 );
