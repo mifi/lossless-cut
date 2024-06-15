6af60a5749de8fbd9405746d44a8fcda9698c88e
nieve <ppu7haxj@duck.com>
2024-06-15 18:58:39 -0400

    true keyframe cut

--- src/renderer/src/ffmpeg.ts
+++ src/renderer/src/ffmpeg.ts
@@ -109,7 +109,7 @@ export const findNextKeyframe = (keyframes: Frame[], time: number) => keyframes.
 const findPreviousKeyframe = (keyframes: Frame[], time: number) => keyframes.findLast((keyframe) => keyframe.time <= time);
 const findNearestKeyframe = (keyframes: Frame[], time: number) => minBy(keyframes, (keyframe) => Math.abs(keyframe.time - time));
 
-export type FindKeyframeMode = 'nearest' | 'before' | 'after';
+export type FindKeyframeMode = 'nearest' | 'before' | 'after' | 'keyframeCutFix';
 
 function findKeyframe(keyframes: Frame[], time: number, mode: FindKeyframeMode) {
   switch (mode) {
@@ -121,6 +121,9 @@ function findKeyframe(keyframes: Frame[], time: number, mode: FindKeyframeMode) 
     }
     case 'after': {
       return findNextKeyframe(keyframes, time);
+    }
+    case 'keyframeCutFix': {
+      return findNextKeyframe(keyframes,time);
     }
     default: {
       return undefined;
