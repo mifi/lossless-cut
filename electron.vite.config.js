d76829b35bcbec114ce7648043638d0d91ddcc4c
Mikael Finstad <finstaden@gmail.com>
2024-07-20 16:05:33 +0200

    upgrade vite deps
    and dedupe

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -8,16 +8,25 @@ export default defineConfig({
     // For the main process and preload, the best practice is to externalize dependencies and only bundle our own code.
     // However, until we use ESM for electron main, we need to include ESM-only deps in the bundle: (exclude from externalize)
     plugins: [externalizeDepsPlugin({ exclude: ['p-map', 'execa', 'nanoid'] })],
+    build: {
+      target: 'node18.17',
+      sourcemap: true,
+    },
   },
   preload: {
     // https://electron-vite.org/guide/dev#dependencies-vs-devdependencies
     plugins: [externalizeDepsPlugin({ exclude: [] })],
+    build: {
+      target: 'node18.17',
+      sourcemap: true,
+    },
   },
   renderer: {
     plugins: [react()],
     build: {
-      chunkSizeWarningLimit: 3e6,
+      target: 'chrome118',
       sourcemap: true,
+      chunkSizeWarningLimit: 3e6,
     },
     server: {
       port: 3001,
